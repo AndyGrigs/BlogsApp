559ff8a71bd5687af1c1d18a8a82e2dda007fa7f
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2023-03-21 19:19:39 +0000

    adding router navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,23 @@
 
 import React from 'react'
 import './App.css'
-
-import PostList from "./Comp/PostList.jsx"
-import PostForm from "./Comp/PostForm.jsx"
-import PostFilter from './Comp/PostFilter.jsx'
-import MyModal from './Comp/modal/MyModal'
-import MyButton from './Comp/button/MyButton'
-import { usePosts } from './Comp/hooks/usePosts'
-import PostService from './Comp/API/PostService'
-import Loader from './Comp/loader/Loader'
-import {useFetching} from  './Comp/hooks/useFetching'
-import {getPagesCount} from './Comp/utils/pages'
-import Pagination from './Comp/pagination/Pagination'
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Posts from './pages/Posts'
+import About from './pages/About'
+import Navbar from './Comp/navbar/Navbar'
 
 
 export default function App() {
-  const [posts, setPosts] = React.useState([])
-  const [totalPages, setTotalPages] = React.useState(0)
-  const [filter, setFilter] = React.useState({ sort: '', query: '' })
-  const [modal, setModal] = React.useState(false)
-  const [limit, setLimit] = React.useState(10)
-  const [page, setPage] = React.useState(1)
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-
-
-  
- const [fetchPosts, isLoading, postError] = useFetching(async ()=> {
-    const responce = await PostService.getAll(limit, page)
-      setPosts(responce.data)
-   const totalCount = responce.headers['x-total-count']
-   setTotalPages(getPagesCount(totalCount, limit))
- })
- 
-  React.useEffect(() => {
-    fetchPosts()
-  }, [page])
-
-
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
-
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
-
-  const changePage  = (page) => {
-    setPage(page)
-  }
-
   return (
     <main>
-      <MyButton onClick={() => setModal(true)}>Add post</MyButton>
-      <MyModal
-        visible={modal}
-        setVisible={setModal}
-      >
-        <PostForm create={createPost} />
-      </MyModal>
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}
-      />
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<About />} />
+          <Route path='/posts' element={<Posts />} />
+        </Routes>
+      </Router>
 
-      <section>
-        {postError && <h1>{postError}</h1>}
-        {
-          isLoading
-          ? <Loader />
-          :  <PostList remove={removePost} posts={sortedAndSearchedPosts} />
-        }
-       <Pagination page={page} totalPages={totalPages} changePage={changePage}/>
-      </section>
     </main>
   )
 }
