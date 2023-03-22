44c6a13096a9a6ee6ef7d8098ef02aa84f78d944
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2023-03-22 13:12:07 +0000

    fixing some bags with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Posts from './pages/Posts'
 import About from './pages/About'
+import Error from './pages/Error';
+
 import Navbar from './Comp/navbar/Navbar'
+
+
 
 
 export default function App() {
   return (
     <main>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path='/' element={<About />} />
           <Route path='/posts' element={<Posts />} />
+          <Route path='*' element={<Error />} />
+
         </Routes>
       </Router>
 
---
+++ src/pages/Error.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function About(){
+  return (
+    <h1 style={{color: 'red'}}> this page doesn't exist
+    </h1>
+  )
+}
